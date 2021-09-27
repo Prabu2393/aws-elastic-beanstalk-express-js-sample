9985d56d90ceb1aba783feccde9a78198fa31dc9
prabu M <callmebluepanther@gmail.com>
2021-09-27 15:05:58 +0530

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Benchmark Enterprises'));
+app.get('/', (req, res) => res.send('Cloud computing is the on-demand availability of computer system resources, especially data storage and computing power, without direct active management by the user. Large clouds often have functions distributed over multiple locations, each location being a data center'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
